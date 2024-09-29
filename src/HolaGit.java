66624bfa50d1c2b6d0fa74b9ce503e4524d45f97
etelletxea <e.tellechea@edu.gva.es>
2024-09-29 10:38:18 +0200

    Realizo un primer cambio

--- src/HolaGit.java
+++ src/HolaGit.java
@@ -1,5 +1,6 @@
 public class HolaGit {
     public static void main(String[] args) {
         System.out.println(" Hola soy Eduardo probando Git");
+        System.out.println(" Realizo un cambio");
     }
 }
