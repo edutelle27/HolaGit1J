4e39a48cf0ef23380b30698b2ecd6e5d8efb9acf
etelletxea <e.tellechea@edu.gva.es>
2024-09-29 12:59:11 +0200

    Muestro valores de i,j,z

--- src/HolaGit.java
+++ src/HolaGit.java
@@ -4,8 +4,9 @@ public class HolaGit {
         System.out.println(" Realizo un cambio");
         System.out.println(" Realizo un segundo cambio");
         System.out.println(" Realizo un tercer cambio");
-        int i=0, j=5;
+        int i=0, j=5, z=9;
         System.out.println(" El valor de i es "+ i);
         System.out.println(" El valor de j es "+ j);
+        System.out.println(" El valor de z es "+ z);
     }
 }
